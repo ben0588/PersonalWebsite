5f17f9e9babecbfad8f3bc3122a14603b65eb24e
ben <energy95272z@gmail.com>
2023-07-06 17:29:24 +0800

    更新 README 說明及優化切換分頁效果

--- personal_website/src/components/layout/Layout.jsx
+++ personal_website/src/components/layout/Layout.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import Header from './Header';
 import Footer from './Footer';
 import GoTopButton from '../common/GoTopButton';
@@ -7,9 +7,10 @@ import { useEffect } from 'react';
 
 const Layout = () => {
     const [isScrolling, setIsScrolling] = useState(false); // 控制是否顯示 TOP 按鈕
-    // 處理 回到頂端 按鈕
+    const { pathname } = useLocation();
+
+    // 監控往下滾動到一定距離才會顯示Top按鈕
     useEffect(() => {
-        // 監控往下滾動到一定距離才會顯示Top按鈕
         window.addEventListener('scroll', () => {
             if (window.scrollY > 400) {
                 setIsScrolling(true);
@@ -27,6 +28,11 @@ const Layout = () => {
             behavior: 'smooth', // 移動效果
         });
     };
+
+    // 偵測當切換分頁時回到頁面最頂端
+    useEffect(() => {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    }, [pathname]);
 
     return (
         <div>
