362cf689be764e3d14e1e6f9e59a450ee51c3b41
ben <energy95272z@gmail.com>
2023-08-05 16:17:47 +0800

    Updates

--- personal_website/src/components/layout/Footer.jsx
+++
@@ -1,8 +0,0 @@
-const Footer = () => {
-    return (
-        <div className='bg bg-black mt-3 mt-sm-5 py-3 '>
-            <p className='text-center fw-bolder text-white mb-0'>2023 © - Developed by Ben0588.</p>
-        </div>
-    );
-};
-export default Footer;
