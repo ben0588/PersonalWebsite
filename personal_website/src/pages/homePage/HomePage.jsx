f73a8055873081c9c8a833afe4c7997b3484db3e
ben0588 <energy95272z@gmail.com>
2025-10-09 09:27:38 +0800

    feat(layout): 更新重導向至新版個人網站連結

--- personal_website/src/pages/homePage/HomePage.jsx
+++ personal_website/src/pages/homePage/HomePage.jsx
@@ -8,12 +8,19 @@ import HomeBiographySection from './HomeBiographySection';
 const HomePage = () => {
     return (
         <>
-            <HomePersonalInfoSection />
+            {/* <HomePersonalInfoSection />
             <HomeSkillSection />
             <HomeWorkSamplesSection />
             <HomeEducationSection />
             <HomeWorkExperienceSection />
-            <HomeBiographySection />
+            <HomeBiographySection /> */}
+
+            <p >
+                正在將您重新導向至我的新版個人網站...
+                <br/><br/>
+                如果頁面沒有自動跳轉，請
+                <a href="https://nextjs-personal-site-pi.vercel.app/">點擊這裡</a>。
+            </p>
         </>
     );
 };
